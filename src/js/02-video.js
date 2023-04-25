bfc92282e8a47dd28df8e67cb65f5e3dab43b51b
DanielPerdomoDev <dfperdomodev@gmail.com>
2023-04-25 04:44:29 -0500

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,39 +1,44 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const video = document.querySelector('vimeo-player');
 const player = new Vimeo.Player(video);
 
-let time; //tiempo en segundos
-if (localStorage.getItem('currentTime')) {
-    time = localStorage.getItem('currentTime');
-} else {
-    time = 0;
-}
-  //Capturando el tiempo con el método getCurrentTime y almacenandolo en el local storage
-  player
-    .on('timeupdate', function (data) {
-      // seconds = the current playback position
-      time = data.seconds;
-      localStorage.setItem('currentTime', time);
-    })
-    .catch(function (error) {
-      // an error occurred
-      console.log(error);
-    });
+player.on('play', function () {
+  console.log('played the video!');
+});
 
-//Colocando el tiempo con el método setCurrentTime, obtenido del local storage
+// let time; //tiempo en segundos
+// if (localStorage.getItem('currentTime')) {
+//     time = localStorage.getItem('currentTime');
+// } else {
+//     time = 0;
+// }
+//   //Capturando el tiempo con el método getCurrentTime y almacenandolo en el local storage
+//   player
+//     .on('timeupdate', throttle(function (data) {
+//       // seconds = the current playback position
+//       time = data.seconds;
+//       localStorage.setItem('currentTime', time);
+//     })
+//     .catch(function (error) {
+//       // an error occurred
+//       console.log(error);
+//     }));
 
-player.setCurrentTime(time).then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
+// //Colocando el tiempo con el método setCurrentTime, obtenido del local storage
 
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+// player.setCurrentTime(time).then(function (seconds) {
+//     // seconds = the actual time that the player seeked to
+//   })
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         // the time was less than 0 or greater than the video’s duration
+//         break;
+
+//       default:
+//         // some other error occurred
+//         break;
+//     }
+//   });
