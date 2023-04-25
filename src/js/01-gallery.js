bcff0ac4faa681cc691017e8614254a22249d0ca
DanielPerdomoDev <dfperdomodev@gmail.com>
2023-04-25 03:12:54 -0500

    test hw 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,46 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
 
-console.log(galleryItems);
+//-------------- Creo arrays del array de objetos para modificar más facilmente con su index --------------//
+let urlPrev = [];
+let urlOpen = [];
+let altImg = [];
+
+galleryItems.map(item => {
+  // Agregar cada propiedad al array correspondiente
+  urlPrev.push(item.preview);
+  urlOpen.push(item.original);
+  altImg.push(item.description);
+});
+
+const gallery = document.querySelector('.gallery');
+
+// ------------ Crear lis html de el arreglo galleryItems-------------//
+
+function createGallery(items) {
+  const itemsList = items
+    .map(
+      image =>
+        `<li  class="gallery__item"><a class="gallery__link" href="${image.original}"><img class="gallery__image" src="${image.preview}" alt="${image.description}"/></a></li>`
+    )
+    .join('');
+
+  return itemsList;
+}
+
+gallery.innerHTML = createGallery(galleryItems);
+
+// -------------------------------------------------------------------//
+// ------------ Función abrir img modal con SimpleLightbox ------------//
+
+gallery.addEventListener('click', event => {
+  event.preventDefault();
+});
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+  captionsData: 'alt',
+  captionDelay: 250,
+});
