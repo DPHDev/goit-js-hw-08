80166b2ae5f10d01c85928c7f3fc9171459edc26
DanielPerdomoDev <dfperdomodev@gmail.com>
2023-04-28 15:37:39 -0500

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -11,7 +11,7 @@ const button = document.querySelector('button[type="submit"]');
 // });
 //--------Revisa si hay info en el locla storage y coloca en el form-------------
 const formSaved = localStorage.getItem('feedback-form-state');
-if (formSaved =! null) {
+if (formSaved != null) {
     input.value = JSON.parse(formSaved).email;
     textArea.value = JSON.parse(formSaved).message;
 };
