d6a343113994c5d483ee406f556645ee724a1809
DanielPerdomoDev <dfperdomodev@gmail.com>
2023-04-28 15:54:28 -0500

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,16 +1,9 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
-// const elements = form.children;
 const input = document.querySelector('input[type="email"]');
 const textArea = document.querySelector('textarea[name="message"]');
 const button = document.querySelector('button[type="submit"]');
-
-// form.addEventListener('click', () => {
-//   console.log(textArea.value);
-// });
-//-------------Objeto default "feedback-form-state" ------------------
-
 
 //--------Revisa si hay info en el locla storage y coloca en el form-------------
 const formSaved = localStorage.getItem('feedback-form-state');
@@ -26,21 +19,20 @@ function setLocalStorage() {
       email: input.value,
       message: textArea.value,
     };
-  // formObject.email = ,
-  // formObject.message = ;
-  // // console.log(input.value);
-  // // console.log(textArea.value);
   localStorage.setItem('feedback-form-state', JSON.stringify(formObject));
-  // console.log(formObject);
+
 }
 
-function clearValues() {
-  console.log(`Los Datos enviados son: \bemail: ${input.value} \bMensaje: ${textArea.value}`);
+const clearValues = (event) => {
+  event.preventDefault();
+  console.log(`Los Datos enviados son: email: ${input.value} Mensaje: ${textArea.value}`);
   localStorage.removeItem('feedback-form-state');
+  input.value = "";
+  textArea.value = "";
 };
 
 
 //--------------------- Llamados a funciones -------------------------
 form.addEventListener('input', throttle(setLocalStorage, 5000));
-button.addEventListener('submit', clearValues());
+button.addEventListener('submit', clearValues);
 
